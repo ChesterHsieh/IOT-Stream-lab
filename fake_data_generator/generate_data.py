2403c5af32b7a94a46c6665572349acd3df0e83b
ChesterHsieh <chsieh5@hawk.iit.edu>
2024-07-25 14:56:31 +0800

    [DATA]mod fake data

--- fake_data_generator/generate_data.py
+++ fake_data_generator/generate_data.py
@@ -5,32 +5,45 @@ from faker import Faker
 
 fake = Faker()
 
-def generate_fake_order():
+
+def generate_fake_weather_sensor_data():
     return {
-        'orderId': fake.uuid4(),
-        'userId': fake.random_int(min=1, max=1000),
-        'productId': fake.random_int(min=1, max=1000),
-        'quantity': fake.random_int(min=1, max=10),
-        'price': round(fake.random_number(digits=5), 2),
-        'orderDate': fake.date_time_this_year().isoformat()
+        'sensorId': fake.uuid4(),
+        'timestamp': fake.date_time_this_year().isoformat(),
+        'temperature': round(fake.random_number(digits=2), 2),
+        'humidity': fake.random_int(min=0, max=100),
+        'windSpeed': round(fake.random_number(digits=2), 2),
+        'atmosphericPressure': round(fake.random_number(digits=4), 2)
     }
+
+
+def generate_fake_motion_sensor_data():
+    return {
+        'sensorId': fake.uuid4(),
+        'timestamp': fake.date_time_this_year().isoformat(),
+        'motionDetected': fake.boolean(),
+        'motionIntensity': fake.random_int(min=1, max=10)
+    }
+
 
 def main():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='orders')
+    channel.queue_declare(queue='solar_data')
 
     while True:
-        order = generate_fake_order()
+        sensor_data = generate_fake_weather_sensor_data()
+        motion_data = generate_fake_motion_sensor_data()
         channel.basic_publish(exchange='',
-                              routing_key='orders',
-                              body=json.dumps(order))
-        print(f"Order sent: {order}")
+                              routing_key='weather',
+                              body=json.dumps(sensor_data))
+        channel.basic_publish(exchange='', routing_key='motion', body=json.dumps(motion_data))
         time.sleep(1)
 
     connection.close()
+
 
 if __name__ == '__main__':
     main()
